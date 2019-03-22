5febb4cd39f4da360e87d67aaf8724f48409e0bf
Your Name <you@example.com>
2019-03-22 11:08:40 +0800

    'ddd'

--- shandian/models/models_mongo.py
+++ shandian/models/models_mongo.py
@@ -1,9 +1,12 @@
 from mongoengine import *
+
 connect('tjj_firewall', host='192.168.30.130', port=27017)
+
+
 class user_property(Document):
     user_id = LongField()
     qcloud_level = IntField()
-    qcloud_risk_type=ListField()
+    qcloud_risk_type = ListField()
+
     def __str__(self):
         return str(self.qcloud_level)
-
